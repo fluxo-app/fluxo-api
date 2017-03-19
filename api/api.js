ad326ccf3193f4afe47c5b0f7b91d15e6b543a3a
Hugo Häggmark <hugo.haggmark@gmail.com>
2017-03-19 13:48:09 +0100

    test

--- api/api.js
+++ api/api.js
@@ -10,6 +10,32 @@ const getCacheKey = (resourceUrl, oauth_access_token, oauth_access_token_secret)
   return `${resourceUrl}|${oauth_access_token}|${oauth_access_token_secret}`
 }
 
+const doRequest = (resourceUrl, req, callback) => {
+  const key = getCacheKey(resourceUrl, req.session.oauth_access_token, req.session.oauth_access_token_secret)
+  let resource = fluxoCache.get(key)
+  if (resource) {
+    callback({
+      status: 200,
+      resource: resource
+    })
+  } else {
+    const oa = oauthHelper.getOAuthFromRequest(req)
+    oa.getProtectedResource(resourceUrl, "GET", req.session.oauth_access_token, req.session.oauth_access_token_secret, (error, data, response) => {
+      if (error) {
+        callback({
+          status: error.statusCode,
+          resource: error.data
+        })
+      }
+      resource = JSON.parse(data)
+      fluxoCache.set(key, resource)
+      callback({
+        status: 200,
+        resource: resource
+      })
+    })
+  }
+}
 const doTrelloRequest = (resourceUrl, req, res) => {
   const key = getCacheKey(resourceUrl, req.session.oauth_access_token, req.session.oauth_access_token_secret)
   let resource = fluxoCache.get(key)
@@ -50,7 +76,23 @@ router.get("/boards/:boardid/lists", (req, res) => {
 })
 
 router.get("/lists/:listid", (req, res) => {
-  doTrelloRequest("https://trello.com/1/lists/" + req.params.listid + "/cards/?actions=updateCard:idList,createCard,copyCard,convertToCardFromCheckItem&filter=all&fields=name,labels,actions", req, res)
+  doRequest("https://trello.com/1/lists/" + req.params.listid, req, (list) => {
+    if (list.status !== 200) {
+      return res.status(list.status).send(list)
+    }
+    doRequest("https://trello.com/1/lists/" + req.params.listid + "/cards/?actions=updateCard:idList,createCard,copyCard,convertToCardFromCheckItem&filter=all&fields=name,labels,actions", req, (cards) => {
+      if (cards.status !== 200) {
+        return res.status(cards.status).send(cards)
+      }
+
+      const result = {
+        id: list.resource.id,
+        name: list.resource.name,
+        cards: cards.resource
+      }
+      res.status(200).send(result)
+    })
+  })
 })
 
 router.get("/cards/:cardid", (req, res) => {
