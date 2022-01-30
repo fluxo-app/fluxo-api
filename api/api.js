94dfa4dec6f688aac9c9c043111df9854fa4d525
Hugo Häggmark <hugo.haggmark@gmail.com>
2022-01-30 10:55:58 +0100

    Chore: fixes bug with dubbles slash in url

--- api/api.js
+++ api/api.js
@@ -1,89 +1,88 @@
-const express = require('express')
-const router = express.Router()
-const settings = require('../settings')
-const NodeCache = require("node-cache")
-const resourceCache = new NodeCache({stdTTL: 300, checkperiod: 30})
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const router = express.Router();
+const settings = require("../settings");
+const NodeCache = require("node-cache");
+const resourceCache = new NodeCache({ stdTTL: 300, checkperiod: 30 });
+const jwt = require("jsonwebtoken");
 
 const getTokenFromRequest = (req) => {
   if (req.query.token) {
-    return req.query.token
+    return req.query.token;
   }
 
-  const authorization = req.headers['authorization']
+  const authorization = req.headers["authorization"];
   if (authorization) {
-    return authorization.replace('Bearer ', '')
+    return authorization.replace("Bearer ", "");
   }
-}
+};
 
 router.use((req, res, next) => {
-  const token = getTokenFromRequest(req)
+  const token = getTokenFromRequest(req);
   try {
-    const decoded = jwt.verify(token, settings.appSecret)
-    req.oauth_access_token = decoded.accessToken
-    req.oauth_access_token_secret = decoded.accessTokenSecret
-  } catch(err) {
+    const decoded = jwt.verify(token, settings.appSecret);
+    req.oauth_access_token = decoded.accessToken;
+    req.oauth_access_token_secret = decoded.accessTokenSecret;
+  } catch (err) {
     return res.status(401).send({
-      signInUrl: `${settings.appUrl}/oauth/login`
-    })
+      signInUrl: `${settings.appUrl}/oauth/login`,
+    });
   }
-  return next()
-})
+  return next();
+});
 
 const getProtectedTrelloResource = (resourceUrl, req) => {
-  const accessToken = req.oauth_access_token
-  const accessTokenSecret = req.oauth_access_token_secret
-  const cacheKey = `${resourceUrl}|${accessToken}|${accessTokenSecret}`
+  const accessToken = req.oauth_access_token;
+  const accessTokenSecret = req.oauth_access_token_secret;
+  const cacheKey = `${resourceUrl}|${accessToken}|${accessTokenSecret}`;
 
   return new Promise((resolve, reject) => {
-    const cachedResource = resourceCache.get(cacheKey)
+    const cachedResource = resourceCache.get(cacheKey);
     if (cachedResource) {
       return resolve({
         statusCode: 200,
-        resource: cachedResource
-      })
+        resource: cachedResource,
+      });
     }
 
-    settings.oauth.getProtectedResource(resourceUrl,
-      "GET",
-      accessToken,
-      accessTokenSecret,
-      (error, data, response) => {
-        if (error) {
-          return resolve({
-            statusCode: error.statusCode,
-            resource: {message:error.data}
-          })
-        }
-
-        const json = JSON.parse(data)
-        resourceCache.set(cacheKey, json)
+    settings.oauth.getProtectedResource(resourceUrl, "GET", accessToken, accessTokenSecret, (error, data, response) => {
+      if (error) {
         return resolve({
-          statusCode: 200,
-          resource: json
-        })
-      })
-  })
-}
+          statusCode: error.statusCode,
+          resource: { message: error.data },
+        });
+      }
+
+      const json = JSON.parse(data);
+      resourceCache.set(cacheKey, json);
+      return resolve({
+        statusCode: 200,
+        resource: json,
+      });
+    });
+  });
+};
 
 router.get("/me", (req, res) => {
-  getProtectedTrelloResource('https://api.trello.com/1/members/me', req)
-    .then(response => res.status(response.statusCode).send(response.resource))
-})
+  getProtectedTrelloResource("https://api.trello.com/1/members/me", req).then((response) => res.status(response.statusCode).send(response.resource));
+});
 
 router.get("/my/boards", (req, res) => {
-  getProtectedTrelloResource('https://trello.com/1/members/my/boards', req)
-    .then(response => res.status(response.statusCode).send(response.resource))
-})
+  getProtectedTrelloResource("https://trello.com/1/members/my/boards", req).then((response) =>
+    res.status(response.statusCode).send(response.resource)
+  );
+});
 
 router.get("/boards/:boardid/lists", (req, res) => {
-  getProtectedTrelloResource(`https://trello.com/1/boards/${req.params.boardid}/lists`, req)
-    .then(response => res.status(response.statusCode).send(response.resource))
-})
+  getProtectedTrelloResource(`https://trello.com/1/boards/${req.params.boardid}/lists`, req).then((response) =>
+    res.status(response.statusCode).send(response.resource)
+  );
+});
 
 router.get("/lists/:listid", (req, res) => {
-  getProtectedTrelloResource(`https://trello.com/1/lists/${req.params.listid}//cards/?actions=updateCard:idList,createCard,copyCard,convertToCardFromCheckItem&filter=all&fields=name,labels,actions`, req)
-    .then(response => res.status(response.statusCode).send(response.resource))
-})
+  getProtectedTrelloResource(
+    `https://trello.com/1/lists/${req.params.listid}/cards/?actions=updateCard:idList,createCard,copyCard,convertToCardFromCheckItem&filter=all&fields=name,labels,actions`,
+    req
+  ).then((response) => res.status(response.statusCode).send(response.resource));
+});
 
-module.exports = router
+module.exports = router;
--- settings.js
+++ settings.js
@@ -1,18 +1,15 @@
-const OAuth = require('oauth').OAuth
-const appKey = process.env.FLUXOAPPKEY
-const appSecret = process.env.FLUXOAPPSECRET
-const appPort = Number(process.env.PORT || 3100)
-const appUrl = process.env.FLUXOAPPURL || "http://localhost:" + appPort
-const requestTokenUrl = "https://trello.com/1/OAuthGetRequestToken"
-const accessURL = "https://trello.com/1/OAuthGetAccessToken"
-const authorizeURL = "https://trello.com/1/OAuthAuthorizeToken"
-const oauth = new OAuth(requestTokenUrl,
-  accessURL,
-  appKey,
-  appSecret,
-  "1.0",
-  null,
-  "HMAC-SHA1")
+/* eslint-disable no-console */
+require("dotenv").config();
+
+const OAuth = require("oauth").OAuth;
+const appKey = process.env.FLUXOAPPKEY;
+const appSecret = process.env.FLUXOAPPSECRET;
+const appPort = Number(process.env.PORT || 3100);
+const appUrl = process.env.FLUXOAPPURL || "http://localhost:" + appPort;
+const requestTokenUrl = "https://trello.com/1/OAuthGetRequestToken";
+const accessURL = "https://trello.com/1/OAuthGetAccessToken";
+const authorizeURL = "https://trello.com/1/OAuthAuthorizeToken";
+const oauth = new OAuth(requestTokenUrl, accessURL, appKey, appSecret, "1.0", null, "HMAC-SHA1");
 
 module.exports = {
   appName: "Fluxo",
@@ -20,5 +17,5 @@ module.exports = {
   appPort: appPort,
   appUrl: appUrl,
   authorizeURL: authorizeURL,
-  oauth: oauth
-}
+  oauth: oauth,
+};
